a6c1cb0ec823a827ddf565b0d08346820ce4d29c
Quake4 <batsov@yandex.ru>
2023-07-25 21:40:15 +0300

    add trackinfo interface

---
+++ source/SOV.NAudio/SOV.NAudio/ITrackInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace SOV.NAudio
+{
+	public class TrackInfo
+	{
+		public int Number;
+		public string Title;
+		public string Performer;
+	}
+
+	public interface ITrackInfo
+	{
+		TrackInfo TrackInfo { get; }
+		int TrackCount { get; }
+	}
+}
