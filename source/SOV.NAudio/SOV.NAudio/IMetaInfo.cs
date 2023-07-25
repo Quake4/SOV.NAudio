d6e76550445a326205613a0d159cc033ff7b67ec
Quake4 <batsov@yandex.ru>
2023-07-25 22:43:10 +0300

    rename itrackinfo to imetainfo

---
+++ source/SOV.NAudio/SOV.NAudio/IMetaInfo.cs
@@ -0,0 +1,33 @@
+﻿/*
+
+Copyright © 2023 Oleg Samsonov aka Quake4. All rights reserved.
+https://github.com/Quake4/SOV.NAudio
+
+This Source Code Form is subject to the terms of the Mozilla
+Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+*/
+namespace SOV.NAudio
+{
+	public class TrackInfo
+	{
+		public int Number;
+		public string Title;
+		public string Performer;
+	}
+
+	public class FileInfo
+	{
+		public int Year;
+		public string Title;
+		public string Artist;
+	}
+
+	public interface IMetaInfo
+	{
+		FileInfo FileInfo { get; }
+		TrackInfo TrackInfo { get; }
+		int TrackCount { get; }
+	}
+}
