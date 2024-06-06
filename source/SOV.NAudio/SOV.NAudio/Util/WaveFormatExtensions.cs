12342f3c959d09d9a4e8d02668ab0666ea1fb791
Quake4 <batsov@yandex.ru>
2024-06-06 13:41:57 +0300

    fix get type

--- source/SOV.NAudio/SOV.NAudio/Util/WaveFormatExtensions.cs
+++ source/SOV.NAudio/SOV.NAudio/Util/WaveFormatExtensions.cs
@@ -1,4 +1,14 @@
-﻿using System.Reflection;
+﻿/*
+
+Copyright © 2023 Oleg Samsonov aka Quake4. All rights reserved.
+https://github.com/Quake4/SOV.NAudio
+
+This Source Code Form is subject to the terms of the Mozilla
+Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+*/
+using System.Reflection;
 
 namespace NAudio.Wave
 {
@@ -9,7 +19,7 @@ namespace NAudio.Wave
 		public static WaveFormat SetEncoding(this WaveFormat format, WaveFormatEncoding encoding)
 		{
 			if (_fiEncoding == null)
-				_fiEncoding = format.GetType().GetField("waveFormatTag", BindingFlags.NonPublic | BindingFlags.Instance);
+				_fiEncoding = typeof(WaveFormat).GetField("waveFormatTag", BindingFlags.NonPublic | BindingFlags.Instance);
 			_fiEncoding.SetValue(format, encoding);
 			return format;
 		}
